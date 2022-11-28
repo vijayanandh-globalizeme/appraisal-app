0084cb61cf7b964a826013ec6ac75d83d5d51dbf
Vijay Anandh <vijay.anandh@globalizeme.com>
2022-11-28 21:09:40 +0530

    new build

---
+++ www/2680.7a3bdce667c44361.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[2680],{2680:(o,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>r});var s=e(5861),a=e(7423);class r extends a.Uw{show(_){return(0,s.Z)(function*(){})()}hide(_){return(0,s.Z)(function*(){})()}}}}]);
